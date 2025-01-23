c1356eb91e1eeaa00fb1a1c94a5a1b70600c5105
Ghanendra K Das <gkdas2@illinois.edu>
2025-01-22 20:03:20 -0500

    Add dstrain increment test example

--- test.cu
+++ test.cu
@@ -108,15 +108,27 @@ void test_strainIncrement()
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  material.calculate_f_internal(
-      element_xloc, element_dof, element_old_stress, element_plastic_strain_eq,
-      element_plastic_strain_rate, element_yield_stress, element_old_gamma,
-      element_strain_increment, delta_T, T_current, gamma_cummulate,
-      internal_energy, inelastic_energy, f_internal);
+
+  int number_of_increments = 1000;
+  for (int i = 0; i < number_of_increments; i++)
+  { 
+    std::cout << "\n Strain increment step: " << i << "\n"; 
+    material.calculate_f_internal(
+        element_xloc, element_dof, element_old_stress, element_plastic_strain_eq,
+        element_plastic_strain_rate, element_yield_stress, element_old_gamma,
+        element_strain_increment, delta_T, T_current, gamma_cummulate,
+        internal_energy, inelastic_energy, f_internal);
+
+    element_dof[0] += 0.00001;
+    element_dof[3] += 0.00001;
+  };  
+            
   auto end = std::chrono::high_resolution_clock::now();
 
   std::chrono::duration<double> elapsed = end - start;
   std::cout << "Strain test Elapsed time: " << elapsed.count() << " seconds" << std::endl;
+
+
 }
 
 
@@ -194,6 +206,109 @@ void test_Fuselage(bool smoke_test)
 
 
 
+/**
+ * @brief Here, we consider a single tetrahedral element
+ *
+ */
+void test_dstrIncrement()
+{
+  // Material Properties
+  T E = 206.9E9; // Pa
+  T rho = 7800;  // kg/m3
+  T nu = 0.33;
+  T Y0 = 806E6;
+  T cp = 0.9; // J/kgC
+  T B = 614E6;
+  T C = 0.0086E6;
+  T M = 1.1;
+  T N = 0.168;
+  T T0 = 20;   // deg C
+  T TM = 1540; // deg C
+  T ref_strain_rate = 1;
+  T taylor_quinney = 0.5;
+
+  std::string name = "Steel 42CrMo4";
+
+  Material material(E, rho, nu, Y0, cp, B, C, M, N, T0, TM, ref_strain_rate,
+                    taylor_quinney, name.c_str());
+  printf("Material: %s\n", name.c_str());
+
+  printf("Begin JC test for 1 element\n");
+  double dt = 1e-7;
+
+  // Initial coordinates of the nodes
+  T element_xloc[12] = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 1.0};
+
+  // Displacement of the nodes
+  T element_dof[12] = {
+      0.0000,
+      0.0,
+      0.0,
+      0.0000,
+      0.0,
+      0.0,
+      0,
+      0.0,
+      0.0,
+      0,
+      0.0,
+      0.0,
+  };
+
+  //
+  T f_internal[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0, 0.0, 0.0};
+
+  // For 5 integration points
+  T element_old_stress[6 * 5] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+  T element_strain_increment[6 * 5] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                       0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                       0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                       0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  T element_plastic_strain_eq[5] = {0.000, 0.000, 0.000, 0.000, 0.000};
+  T element_plastic_strain_rate[5] = {0.000, 0.000, 0.000, 0.000, 0.000};
+  T element_yield_stress[5] = {806E6, 806E6, 806E6, 806E6, 806E6};
+  T element_old_gamma[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+  T delta_T = dt;
+  T T_current[5] = {20, 20, 20, 20, 20};
+  T gamma_cummulate[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+  T internal_energy[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+  T inelastic_energy[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+  
+
+  int number_of_increments = 5000;
+  for (int i = 0; i < number_of_increments; i++)
+  { 
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "\n Strain increment step: " << i << "\n"; 
+    material.calculate_f_internal_host_dstr(
+        element_xloc, element_dof, element_old_stress, element_plastic_strain_eq,
+        element_plastic_strain_rate, element_yield_stress, element_old_gamma,
+        element_strain_increment, delta_T, T_current, gamma_cummulate,
+        internal_energy, inelastic_energy, f_internal);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "\t Strain test Elapsed time: " << elapsed.count() << " seconds" << std::endl;
+
+    element_strain_increment[0] = 0.001;
+    element_strain_increment[6] = 0.001;
+    element_strain_increment[12] = 0.001;
+    element_strain_increment[18] = 0.001;
+    element_strain_increment[24] = 0.001;
+  };  
+            
+  
+
+}
+
+
 int main(int argc, char *argv[])
 {
   constexpr int dof_per_node = 3;
@@ -213,8 +328,8 @@ int main(int argc, char *argv[])
     }
   }
 
-
-  test_strainIncrement();
+  test_dstrIncrement();
+  //test_strainIncrement();
 
   //test_Fuselage(smoke_test);
 
